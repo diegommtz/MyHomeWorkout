a112032213d554f43b8b0f0de599e8b5074f73be
diego.mmtz <diego.mmtz@hotmail.com>
2020-10-08 16:30:59 -0500

    Camera added but image is shown small

--- app/src/main/java/mx/tec/myhomeworkout/PrevPhotos.kt
+++ app/src/main/java/mx/tec/myhomeworkout/PrevPhotos.kt
@@ -1,6 +1,9 @@
 package mx.tec.myhomeworkout
 
+import android.app.ActionBar
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.ImageDecoder
@@ -8,34 +11,61 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_prev_photos.*
 import java.lang.Exception
 
 class PrevPhotos : AppCompatActivity() {
 
-    val selectPhoto: Int = 1
+    val selectFile: Int = 0
+    val requestCamera = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_prev_photos)
 
         pickerFoto.setOnClickListener{
-            val i = Intent(Intent.ACTION_PICK)
-            i.type = "image/*"
-            startActivityForResult(i, selectPhoto)
+            SelectImage()
         }
     }
 
+    fun SelectImage()
+    {
+        val items = arrayOf("Camera", "Gallery", "Cancel")
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Elige una opción")
+        builder.setItems(items) { dialog, which ->
+            when(items[which]) {
+                "Camera" -> {
+                    val i = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                    startActivityForResult(i, requestCamera)
+                }
+                "Gallery" -> {
+                    val i = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    i.type = "image/*"
+                    startActivityForResult(Intent.createChooser(i, "Selecciona una imagen"), selectFile)
+                }
+                "Cancel" -> {
+                    dialog.dismiss()
+                }
+            }
+        }
+        builder.show()
+     }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == selectPhoto && resultCode == Activity.RESULT_OK && data != null && data.data != null) {
-            val uri = data.data
-            try {
-                val bitMap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
-                imagen.setImageBitmap(bitMap)
-            } catch (e: Exception)
+        if(resultCode == Activity.RESULT_OK)
+        {
+            if(requestCode == requestCamera){
+                val bundle: Bundle = data?.extras!!
+                val bmp: Bitmap = bundle.get("data") as Bitmap
+                imagen.setImageBitmap(bmp)
+            }
+            else if (requestCode == selectFile)
             {
-                e.printStackTrace()
+                val uri = data?.data
+                imagen.setImageURI(uri)
             }
         }
     }
